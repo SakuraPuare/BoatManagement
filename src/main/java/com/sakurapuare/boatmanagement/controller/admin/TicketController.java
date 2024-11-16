3db2ac29a5dd32905991eb66e9c21dbd4f8f890a
Steven Moder <java20131114@gmail.com>
2024-11-16 20:51:56 +0800

    refactor: 移动控制器

--- src/main/java/com/sakurapuare/boatmanagement/controller/admin/TicketController.java
+++ src/main/java/com/sakurapuare/boatmanagement/controller/admin/TicketController.java
@@ -1,11 +1,19 @@
-package com.sakurapuare.boatmanagement.controller;
+package com.sakurapuare.boatmanagement.controller.admin;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.mybatisflex.core.paginate.Page;
 import com.sakurapuare.boatmanagement.pojo.entity.Ticket;
 import com.sakurapuare.boatmanagement.service.TicketService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 /**
  * 控制层。
