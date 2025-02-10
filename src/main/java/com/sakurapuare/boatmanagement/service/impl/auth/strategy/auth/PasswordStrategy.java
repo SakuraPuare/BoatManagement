2ccf57d6bb431418075443e371d38c7d1173a53c
Steven Moder <java20131114@gmail.com>
2025-02-11 02:50:11 +0800

    fix: 更新密码策略以支持用户注册和登录逻辑，添加账号存在性检查

--- src/main/java/com/sakurapuare/boatmanagement/service/impl/auth/strategy/auth/PasswordStrategy.java
+++ src/main/java/com/sakurapuare/boatmanagement/service/impl/auth/strategy/auth/PasswordStrategy.java
@@ -2,13 +2,13 @@ package com.sakurapuare.boatmanagement.service.impl.auth.strategy.auth;
 
 import com.mybatisflex.core.query.QueryWrapper;
 import com.sakurapuare.boatmanagement.constant.auth.AuthStatus;
+import com.sakurapuare.boatmanagement.constant.auth.AuthType;
 import com.sakurapuare.boatmanagement.mapper.AccountsMapper;
 import com.sakurapuare.boatmanagement.pojo.dto.AuthRequestDTO;
 import com.sakurapuare.boatmanagement.pojo.entity.Accounts;
 import com.sakurapuare.boatmanagement.pojo.entity.table.AccountsTableDef;
 
 public class PasswordStrategy implements AuthStrategy {
-
 
     private static final AccountsTableDef accountsTableDef = new AccountsTableDef();
 
@@ -23,16 +23,36 @@ public class PasswordStrategy implements AuthStrategy {
         String username = authRequestDTO.getUsername();
         String password = authRequestDTO.getPassword();
 
-
         String field = AuthStrategy.getFieldName(status);
 
-        Accounts account = accountsMapper.selectOneByQuery(
-                QueryWrapper.create()
-                        .eq(field, username)
-                        .eq(accountsTableDef.PASSWORD.getName(), password));
 
-        if (account == null) {
-            throw new IllegalArgumentException("账号或密码错误");
+        Accounts account = null;
+        if (status.getType() == AuthType.LOGIN) {
+            account = accountsMapper.selectOneByQuery(
+                    QueryWrapper.create()
+                            .eq(field, username)
+                            .eq(accountsTableDef.PASSWORD.getName(), password));
+
+            if (account == null) {
+                throw new IllegalArgumentException("账号或密码错误");
+            }
+        } else {
+            account = accountsMapper.selectOneByQuery(
+                    QueryWrapper.create()
+                            .eq(field, username));
+
+            if (account != null) {
+                throw new IllegalArgumentException("账号已存在");
+            }
+
+            account = Accounts.builder()
+                    .username(username)
+                    .password(password)
+                    .isActive(true)
+                    .isBlocked(false)
+                    .build();
+
+            accountsMapper.insertSelective(account);    
         }
 
         return account;
