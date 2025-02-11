6aa714167b0f43aac5005f6a52eaaa043a16e3f8
Steven Moder <java20131114@gmail.com>
2025-02-12 02:22:02 +0800

    chore: Update run configurations and generation script
    - Remove unnecessary build and database import steps from generate.sh
    - Simplify script to focus on DTO/VO generation
    - Update SQLGen run configuration by removing external tool task
    - Add new run configuration for code generation script

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,13 +1,3 @@
-
-./mvnw clean install package -DskipTests
-
-# 导入sql
-mariadb -u root -p -h 127.0.0.1 -P 3306 -D boatmanagement < scripts/database.sql
-
-# GenSQL
-
-/usr/lib/jvm/openjdk-zulu21-ca-fx-bin/bin/java  -jar target/SQLGen-jar-with-dependencies.jar
-
 # 执行sh文件
 sh scripts/generate_dto_vo.sh
 
