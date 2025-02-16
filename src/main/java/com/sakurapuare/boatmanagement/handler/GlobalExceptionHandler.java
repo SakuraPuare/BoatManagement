23cdec52c8da91c1482e9a7200b57003e215ca98
Steven Moder <java20131114@gmail.com>
2025-02-16 14:14:28 +0800

    feat: Enable Spring Boot Actuator and improve global exception logging
    - Uncommented and simplified Spring Boot Actuator dependency in pom.xml
    - Updated GlobalExceptionHandler to use SLF4J logging instead of printStackTrace()
    - Added @Slf4j annotation for more robust error logging

--- src/main/java/com/sakurapuare/boatmanagement/handler/GlobalExceptionHandler.java
+++ src/main/java/com/sakurapuare/boatmanagement/handler/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package com.sakurapuare.boatmanagement.handler;
 
 import com.sakurapuare.boatmanagement.common.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice(basePackages = "com.sakurapuare.boatmanagement.controller")
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public Response<String> handleException(Exception e) {
-        e.printStackTrace();
+        // e.printStackTrace();
+        log.error("全局异常：", e);
         return Response.error(e.getMessage());
     }
 }
