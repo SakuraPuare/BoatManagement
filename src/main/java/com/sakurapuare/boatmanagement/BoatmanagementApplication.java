13664de6d6382cac4f699de6c041a89012172797
Steven Moder <java20131114@gmail.com>
2025-05-29 14:09:56 +0800

    fix: 更新依赖版本和配置文件
    - 更新 pom.xml 中的多个依赖版本以提高兼容性和安全性，包括 MyBatis Flex、HikariCP、Hutool、Mockito、JUnit 和 Springdoc OpenAPI。
    - 移除 BoatmanagementApplication.java 中的 MapperScan 注解以简化配置。
    - 更新 .run.xml 文件中的数据源配置，添加命名空间以确保正确的数据库连接。
    - 删除 BoatmanagementApplicationTests.java 测试文件以清理不再使用的测试代码。

--- src/main/java/com/sakurapuare/boatmanagement/BoatmanagementApplication.java
+++ src/main/java/com/sakurapuare/boatmanagement/BoatmanagementApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication
-@MapperScan("com.sakurapuare.boatmanagement.mapper")
 public class BoatmanagementApplication {
 
     public static void main(String[] args) {
